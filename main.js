dcb6f756cb8c00451a663318a253636e6abf15f5
Brian Travis <bztravis88@gmail.com>
2024-02-09 22:30:57 -0500

    Add objects

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ const nothing = null;
 
 const sahil = {
   age: 19,
+  color: 'brown',
   gains: 0,
   money: -6969,
   bitches: null,
@@ -19,3 +20,14 @@ const sahil = {
 };
 
 console.log(sahil.age);
+sahil.age = 20;
+console.log(sahil);
+
+// primitives are not objects and equality is determined by value
+console.log(20 == 20);
+const num = 1;
+// num = 2; // error
+
+// so how does reassigning Sahil's age work?
+// objects use *referential* equality, they are passed and interpretted by their reference in memory, not their evaluated values
+console.log({ value: 1 } == { value: 1 });
