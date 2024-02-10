8249dd2fa3d51efe7e729d5c9053b99da444908f
Brian Travis <bztravis88@gmail.com>
2024-02-09 22:25:40 -0500

    Add types

---
+++ main.js
@@ -0,0 +1,21 @@
+let mutable = 2;
+mutable = 4;
+
+const str = 'hello world';
+console.log(str);
+
+const bool = true;
+const notBool = !bool;
+
+const myVar = undefined;
+const nothing = null;
+
+const sahil = {
+  age: 19,
+  gains: 0,
+  money: -6969,
+  bitches: null,
+  virgin: true,
+};
+
+console.log(sahil.age);
